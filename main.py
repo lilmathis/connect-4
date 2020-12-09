2be484f9c3634a70163cf7b8977c89426930ea5b
lilspaghettii <ozmaly.dev@gmail.com>
2020-12-09 11:59:50 +0100

    📚Modification de main.py
    - redef de coup_possible()
    - Ajout de jouer()

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-def grille_vide(l,c):# définis une grille ( liste bi-dimensionelle ) sur c par l de longeur
-    g = [[1 for j in range(c)] for i in range(l)]
-    g[0][0] = 0
-    g = ([i for i in g[::-1]])
+def grille_vide(l,c): #définis une grille ( liste bi-dimensionelle ) sur c par l de longeur
+    g = [[0 for j in range(c)] for i in range(l)]
+    g = ([i for i in g[::-1]]) #on inverse les listes (lignes)
     return g
 
-def affiche(g):# affiche le tableau g ligne par ligne
+def affiche(g): #affiche le tableau g ligne par ligne
     print(*g, sep='\n')
 
-def coup_possible(c, g):#affiche le numero de la colonne si une colonne contient un zero
-    for i in range(c):
-        for k in range(6):
-            if g[k][i] == 0:
-                print(i)
-                return True
+def coup_possible(c, g): #renvoie un booléen lorsque un élément dans c
+    if g[len(g)-1][c]==0:
+        return True
 
-# def jouer(g,j,c):
+def jouer(g,j,c): #place le jouer j dans la case libre (si une case est libre) dans la colonne c du tableau g
+    n=0
+    if coup_possible(c, g):
+        while g[n][c] == 0 and n < 5: #
+            n+=1
+        g[n][c] = j
+        return affiche(g)
 
 
 g = grille_vide(6,7)
-print(coup_possible(7,g))
-print(affiche(g))
+print(jouer(g,1,2))
